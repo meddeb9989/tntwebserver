26ff5f38a3be2feaadce2a6e3637bc7e98f9982d
Meddeb Mohamed Fakher <meddebmohamedfakher@MacBook-Pro-de-Meddeb-Mohamed-Fakher.local>
2017-03-09 02:10:42 +0100

    Email Sending V1.0

--- webserver/serverapp/sendemail.py
+++ webserver/serverapp/sendemail.py
@@ -2,40 +2,47 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'meedbmohamedfakher'
-
+import sys  
 import yagmail
 
+reload(sys)  
+sys.setdefaultencoding('utf8')
+
+
 class SendEmail(object):
-    """docstring for Email"""
-    def __init__(self, **kwargs):
-        super(SendEmail, self).__init__(**kwargs)
-
-    def send_email_transaction_valid(self, amount, trader, email_employee, email_trader):
-
-        gmail_user = 'tanndtech@gmail.com'
-        password = 'msif2017'
-        yag = yagmail.SMTP(user=gmail_user, password=password)
-
-        message_employee = "Bonjour Monsieur,\n \n" \
-        + "Votre paiement de €" + amount + " EUR à " + trader + ", a été effectué avec succès. \n \n"\
-        + "Il est possible que la transaction n'apparaisse qu'au bout de quelques minutes sur votre compte. \n\n" \
-        + "Marchand \n"\
-        + trader + "\n"\
-        + email_trader
-        
-        Subject_employee = 'Reçu pour votre paiement à ' + trader 
-
-        message_trader = "Bonjour " +trader +",\n \n" \
-        + "Paiement Reçu  avec succès d'un montant de €" + amount + " EUR. \n \n"\
-        + "Il est possible que la transaction n'apparaisse qu'au bout de quelques minutes sur votre compte. \n\n" \
-        + "AVENTIX \n"\
-        + "TAN & TECH\n"\
-        + gmail_user
-        
-        Subject_trader = 'Paiement Reçu '
-
-        yag.send(email_employee, subject=Subject_employee, contents=message_employee, headers={"From":"TAN & TECH", "To":email_employee})
-        yag.send(email_trader, subject=Subject_trader, contents=message_trader, headers={"From":"TAN & TECH", "To":email_trader})
+      """docstring for Email"""
+      def __init__(self, **kwargs):
+            super(SendEmail, self).__init__(**kwargs)
+  
+      def send_email_transaction_valid(self, amount, trader, email_employee, email_trader):
+  
+            gmail_user = 'tanndtech@gmail.com'
+            password = 'msif2017'
+            yag = yagmail.SMTP(user=gmail_user, password=password)
+            email_employee=str(unicode(email_trader))
+            email_trader=str(unicode(gmail_user))
+            print amount, trader, email_employee, email_trader
+    
+            message_employee = "Bonjour Monsieur,\n \n" \
+            + "Votre paiement de €" + amount + " EUR à " + trader + ", a été effectué avec succès. \n \n"\
+            + "Il est possible que la transaction n'apparaisse qu'au bout de quelques minutes sur votre compte. \n\n" \
+            + "Marchand \n"\
+            + trader + "\n"\
+            + email_trader
+            
+            Subject_employee = 'Reçu pour votre paiement à ' + trader 
+    
+            message_trader = "Bonjour " +trader +",\n \n" \
+            + "Paiement Reçu  avec succès d'un montant de €" + amount + " EUR. \n \n"\
+            + "Il est possible que la transaction n'apparaisse qu'au bout de quelques minutes sur votre compte. \n\n" \
+            + "AVENTIX \n"\
+            + "TAN & TECH\n"\
+            + gmail_user
+            
+            Subject_trader = 'Paiement Reçu '
+    
+            yag.send(email_employee, subject=Subject_employee, contents=message_employee, headers={"From":"TAN & TECH", "To":email_employee})
+            yag.send(email_trader, subject=Subject_trader, contents=message_trader, headers={"From":"TAN & TECH", "To":email_trader})
 
 if __name__ == '__main__':
     email = SendEmail()
