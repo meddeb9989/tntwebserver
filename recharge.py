d77aca4d3c9b23b74cc01ebed79f88cd109c2073
Meddeb Mohamed Fakher <meddebmohamedfakher@MacBook-Pro-de-Meddeb-Mohamed-Fakher.local>
2017-03-24 12:34:49 +0100

    The last version

---
+++ recharge.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests
+
+login = 'admin'
+password = 'msif2017'
+website_url = "https://tntbankserver.herokuapp.com/"
+r = requests.post(website_url+"api-token-auth/", data={"username": login,"password":password})
+token =json.loads(r.text)
+data = [{"valid": "Pas de transactions non valides"}]
+if u'non_field_errors' in token:
+    data = [{'valid' : False}]
+else:
+    token = token[u'token']
+    url=website_url+"recharge_auto/"
+    r=requests.get(url, headers={'Authorization': 'Token '+token})
